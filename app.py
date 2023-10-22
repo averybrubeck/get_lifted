cc5eb21a12c7d591f44994c0c5a0a4347e7ca889
averybrubeck <averybrubeck@yahoo.com>
2023-10-22 12:23:15 -0400

    more setup

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-import re
+from flask import Flask
+from flask import render_template
 from datetime import datetime
-
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Flask!"
+    return render_template("home.html")
 
-@app.route("/index")
-@app.route("/index/<name>")
-def hello_there(name = None):
-    return render_template(
-        "index.html",
-        name = name,
-        date=datetime.now()
-    )
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
